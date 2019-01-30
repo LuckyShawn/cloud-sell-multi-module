e888056d4de8b3e17df21ea89e0bc4f53b936f3d
LuckyShawn <LuckyShawn@foxmail.com>
2019-01-30 17:35:26 +0800

    测试RestTemplate，三种方式

---
+++ order/src/main/java/com/shawn/order/controller/ClientController.java
@@ -0,0 +1,50 @@
+package com.shawn.order.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Description TODO
+ * @Author shawn
+ * @create 2019/1/30 0030
+ */
+@RestController
+@Slf4j
+public class ClientController {
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    /**
+     * 测试RestTemplate
+     *
+     * @return
+     */
+    @GetMapping("/getProductMsg")
+    public String getProductMsg() {
+        //1.第一种方式：直接使用restTemplate，url写死，地址不灵活
+        //RestTemplate restTemplate = new RestTemplate();
+        //String msg = restTemplate.getForObject("http://localhost:8901/msg",String.class);
+
+
+        //2.第二种方式：利用loadBalancerClient，通过应用名获取url，过于繁杂
+//        ServiceInstance serviceInstance = loadBalancerClient.choose("PRODUCT");
+//        String url = String.format("http://%s%s", serviceInstance.getHost(), ":"+serviceInstance.getPort() + "/msg");
+//        RestTemplate restTemplate = new RestTemplate();
+//        String msg = restTemplate.getForObject(url,String.class);
+
+        //3.第三种方式：利用@LoadBalanced，restTemplate直接使用应用名调用（负载均衡）,此方法最优
+        String msg = restTemplate.getForObject("http://PRODUCT/msg",String.class);
+        log.info("msg={}", msg);
+        return msg;
+    }
+}
